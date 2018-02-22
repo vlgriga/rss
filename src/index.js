1cb95711ced22b6406f4cc96467bb8fadf5ebdf9
vlgriga <vladislavgriga@gmail.com>
2018-02-22 17:14:24 +0300

    Second task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-module.exports = function warmup(temperature) {
-  return temperature * 9 / 5 + 32;
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
 
+module.exports = 
+function getLoveTrianglesCount(preferences = []) {
+  var counter = 0;
+
+  preferences.map( (index,key) => {
+    preferences.map( (index1,key1) => {
+      if( index === key1 + 1 && index !== index1) {
+        preferences.map( (index2, key2) => {
+          if( index1 === key2 + 1 && index2 === key + 1 && index1 !== index2 ) {
+            preferences[key] = 0;
+            preferences[key1] = 0;
+            preferences[key2] = 0;
+            counter++;
+          }
+        })
+      }
+    })
+  });  
+
+  return counter;
 };
+
+
+
