2b02eda8dfc93dbf691836c2f25e95bcbb077696
vlgriga <vladislavgriga@gmail.com>
2018-02-24 16:56:47 +0300

    Completed 5 task

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
-module.exports =
- function solveEquation(equation) {
-  var splArray = equation.split(' ');
-  var sign1 = splArray[3];
-  var sign2 = splArray[7];
-  
+// PLEASE DON'T change function name
+module.exports = 
+function makeExchange(currency) {
+    var obj = {
+        'H' : 0,
+        "Q" : 0,
+        "D" : 0,
+        "N" : 0,
+        "P" : 0
+    };
 
-  var arr =[];
-  var numbers = splArray.filter( index => { 
-    if(!isNaN(+index))  return +index  
-  }) ; 
+    if( currency < 0) {
+        return {};
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+        while( currency >= 50) {
+            currency -= 50;
+            obj["H"]++;
+        }
+        while( currency >= 25) {
+            currency -= 25;
+            obj["Q"]++;
+        }
+        while( currency >= 10) {
+            currency -= 10;
+            obj["D"]++;
+        }
+        while( currency >= 5) {
+            currency -= 5;
+            obj["N"]++;
+        }
+        while( currency >= 1) {
+            currency -= 1;
+            obj["P"]++;
+        }
+    }
 
-  let [a,b,c] = numbers;
-  if(sign1 == '-' ) {
-    b = 0-b;
-  }
-  if(sign2 == '-' ) {
-    c = 0-c;
-  }
-
-  var d =  b * b - (4 * a * c);
-  let x1 = -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
-  let x2 = -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
-
-
-  
-  if( x2 > x1) {
-    arr.push(Math.round( x1));
-    arr.push(Math.round( x2));
-  } else {
-    arr.push(Math.round( x2));
-    arr.push(Math.round( x1));
-  }
- 
-  return arr;
+    for (var key in obj) {
+        if(obj[key] ==   0) {
+            delete obj[key];
+        }
+    }
+   
+    return obj;
 }
 
-//solveEquation('-175 * x^2 - 397148325 * x + 709028410683600');
